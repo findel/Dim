e97c3d5216e2d26d2fa5e35e4cdb2f99c5a0f572
Phil Baines <phil.baines@wubbleyew.com>
2012-12-02 21:26:08 +0000

    Modify output colours and add 'loader' for later use

--- src/DimConsole.cs
+++ src/DimConsole.cs
@@ -8,7 +8,7 @@ namespace Dim
 		
 		public static void WriteIntro(string intro)
 		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
 			Console.WriteLine("#### " + intro + " ####");
 			Console.ResetColor();
 			Console.WriteLine("#");
@@ -29,8 +29,94 @@ namespace Dim
 		
 		public static void WriteInfoLine(string line1)
 		{
-			Console.WriteLine("#    * " + line1);
+			Console.Write("#    ");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("* " + line1);
+			Console.ResetColor();
 			Console.WriteLine("#");
+			
+		}
+		
+		private static int loaderCounter = 0;
+		
+		public static void WriteLoader()
+		{
+			Console.Write("#    ");
+			Console.ForegroundColor = ConsoleColor.Green;
+			loaderCounter++;
+			switch (loaderCounter)
+			{
+				case 1:
+					Console.Write("-         ");
+					break;
+				case 2:
+					Console.Write("=         ");
+					break;
+				case 3:
+					Console.Write("=-        ");
+					break;
+				case 4:
+					Console.Write("==        ");
+					break;
+				case 5:
+					Console.Write("==-       ");
+					break;
+				case 6:
+					Console.Write("===       ");
+					break;
+				case 7:
+					Console.Write("===-      ");
+					break;
+				case 8:
+					Console.Write("====      ");
+					break;
+				case 9:
+					Console.Write("====-     ");
+					break;
+				case 10:
+					Console.Write("=====     ");
+					break;
+				case 11:
+					Console.Write("=====-    ");
+					break;
+				case 12:
+					Console.Write("======    ");
+					break;
+				case 13:
+					Console.Write("======-   ");
+					break;
+				case 14:
+					Console.Write("=======   ");
+					break;
+				case 15:
+					Console.Write("=======-  ");
+					break;
+				case 16:
+					Console.Write("========  ");
+					break;
+				case 17:
+					Console.Write("========- ");
+					break;
+				case 18:
+					Console.Write("========= ");
+					break;
+				case 19:
+					Console.Write("=========-");
+					break;
+				case 20:
+					Console.Write("==========");
+					loaderCounter = 0;
+					break;
+
+			}
+			Console.ResetColor();
+			Console.SetCursorPosition(Console.CursorLeft - 15, Console.CursorTop);
+		}
+		
+		public static void StopSpiner()
+		{
+			Console.Write("               ");
+			Console.SetCursorPosition(Console.CursorLeft - 15, Console.CursorTop);
 		}
 		
 	}
