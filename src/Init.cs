bcae9926650f86802246b31e63591cbea4a8052f
Phil Baines <phil.baines@wubbleyew.com>
2012-12-06 21:09:12 +0000

    Add more required options to 'init' - not using yet, not sure if good idea.

--- src/Init.cs
+++ src/Init.cs
@@ -10,7 +10,33 @@ namespace Dim
 		public Init()
 		{
 			base.IsCommand("init", "Initialise a new Dim project");
+			
+			base.HasRequiredOption("h|host=",
+			                       "The host (name or i.p. address) for your database",
+			                       h => this.Host = h);
+			
+			base.HasRequiredOption("P|port=",
+			                       "The port for you database",
+			                       p => this.Port = p);
+			
+			base.HasRequiredOption("u|username=",
+			                       "The username to connect to your database.",
+			                       u => this.Username = u);
+			
+			base.HasRequiredOption("p|password=",
+			                       "The password to connect to your database.",
+			                       p => this.Password = p);
+			
+			base.HasRequiredOption("s|schema=",
+			                       "The database schema to work with",
+			                       s => this.Schema = s);
 		}
+		
+		private string Host { get; set; }
+		private string Port { get; set; }
+		private string Username { get; set; }
+		private string Password { get; set; }
+		private string Schema { get; set; }
 		
 		public override int Run(string[] remainingArguments)
 		{
