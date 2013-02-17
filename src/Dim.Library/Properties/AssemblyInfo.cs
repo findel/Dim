a5f85202affe98810ad04ae483a6369fc4a6f08f
Phil Baines <phil.baines@wubbleyew.com>
2013-02-17 23:35:09 +0000

    Adding empty projects for Dim.Library and Dim.MySql

--- src/Dim.Library/Properties/AssemblyInfo.cs
+++ src/Dim.Library/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dim")]
+[assembly: AssemblyTitle("Dim.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dim")]
+[assembly: AssemblyProduct("Dim.Library")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/Dim.MySql/Properties/AssemblyInfo.cs
+++ src/Dim.MySql/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dim")]
+[assembly: AssemblyTitle("Dim.MySql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dim")]
+[assembly: AssemblyProduct("Dim.MySql")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
